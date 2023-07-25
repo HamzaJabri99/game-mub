8e921b334c4657840cebf518ce61f6aafa00aa68
unknown <jabri.oldd@gmail.com>
2023-07-25 01:24:32 +0100

    Implement dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, Text, useColorMode } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
       <Text>GameMub</Text>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "dark" : "light"}
+      </Button>
     </HStack>
   );
 };
