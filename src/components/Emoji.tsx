e21bef31dd2db785b22a7693c15be04830e811ba
unknown <jabri.oldd@gmail.com>
2023-07-29 17:16:03 +0100

    Fixing games in playstation platform

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,18 +1,20 @@
 import thumbsUp from "../assets/thumbs-up_1f44d.png";
 import meh from "../assets/unamused-face_1f612.png";
 import bullsEye from "../assets/direct-hit_1f3af.png";
+import thumbsDown from "../assets/thumbs-down_1f44e.png";
 import { Image, ImageProps } from "@chakra-ui/react";
 interface Props {
   rating: number;
 }
 const Emoji = ({ rating }: Props) => {
-  if (rating < 3) return null;
+  if (rating < 3)
+    return <Image src={thumbsDown} alt="not recommended" boxSize="25px" />;
   const emojiMap: { [key: number]: ImageProps } = {
     3: { src: meh, alt: "meh", boxSize: "25px" },
     4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
     5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
   };
-  return <Image {...emojiMap[rating]} marginTop={1}/>;
+  return <Image {...emojiMap[rating]} marginTop={1} />;
 };
 
 export default Emoji;
