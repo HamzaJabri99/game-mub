734e6c03205dd32163a720b4a1d38a9ce6b27ae4
unknown <jabri.oldd@gmail.com>
2023-08-07 22:31:11 +0100

    Handle routing errors

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,13 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <HStack paddingY={4} paddingX={8}>
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
