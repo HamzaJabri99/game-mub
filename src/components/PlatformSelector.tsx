a2a4e8d4aa666f3e0dff8c166fd04392220002d3
unknown <jabri.oldd@gmail.com>
2023-08-09 17:02:24 +0100

    Refactor: Enriry interfaces.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -8,7 +8,7 @@ import {
   MenuList,
 } from "@chakra-ui/react";
 import usePlatforms from "../hooks/usePlatforms";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import usePlatform from "../hooks/usePlatform";
 import useGameQueryStore from "../store";
 
